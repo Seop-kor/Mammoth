d80dec5ebf947745b410d6986bd4c4495248252c
Seop-kor <dkdn1004@naver.com>
2021-07-08 10:14:57 +0900

    0708 두번째 섹션 구현 중

---
+++ js/fullpage_code.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("#fullpage").fullpage({
+
+    anchors : ['1st', '2nd', '3rd'],
+
+  });
+});
--- js/jq.main.js
+++ js/jq.main.js
@@ -24,6 +24,44 @@ $(function(){
   $(".menu_ico").hover(function(){
     $(this).toggleClass('active');
   });
+
+  //Mobile Menu Click Effect
+  $(".menu_ico").click(function(){
+    $(".mobile_nav").animate({left:0}, 300);
+    $("section").animate({left:'60%'}, 300);
+    $(".overlay").animate({
+      "opacity":0.7,
+      "left": "60%",
+      "width":"100%"
+    }, 300);
+  });   
+
+  $(".close_btn, .overlay").click(function(){
+    $(".mobile_nav").animate({left:'-60%'}, 300);
+    $("section").animate({left:0}, 300);
+    $(".overlay").animate({
+      "opacity":0,
+      "left": 0,
+      "width":0
+    }, 300);
+  });
+
+  // Mobile Menu Accordion Effect
+  $(".mobile_tit").click(function(){
+    if($(this).hasClass("active")){
+      $(this).removeClass("active");
+      $(this).siblings(".mobile_sub_nav").slideUp(200);
+      $(this).removeClass("rotate");
+      $(".mobile_tit").removeClass("rotate");
+    } else {
+      $(".mobile_tit").removeClass("active");
+      $(".mobile_tit").removeClass("rotate");
+      $(this).addClass("active");
+      $(".mobile_sub_nav").slideUp(200);
+      $(this).siblings(".mobile_sub_nav").slideDown(200);
+      $(this).addClass("rotate");   
+    }
+  });
   
 });
 
